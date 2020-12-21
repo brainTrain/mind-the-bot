3cb3133b4f0f4c266c14be3b0df4bd01d94e03a3
brainTrain <abductee@gmail.com>
2020-12-21 02:47:22 -0800

    better initial coordinates

--- index.js
+++ index.js
@@ -17,9 +17,9 @@ const options = {
 };
 
 const SPAWN_POINT = {
-  x: -192.84177554267393,
-  y: 71,
-  z: -118.13736571556387,
+  x: -224.570,
+  y: 67,
+  z: -125.879,
 };
 
 const bot = mineflayer.createBot(options);
@@ -27,18 +27,19 @@ bot.loadPlugin(pathfinder);
 
 bot.on('spawn', () => {
   const myPlayer = bot.players[MCMyPlayerUsername];
+  const MCData = initMCData(bot.version);
 
   if (myPlayer) {
     bot.chat('broooooooooooooooooo !!!!!!');
-    followPlayer(bot, myPlayer);
+    followPlayer(bot, myPlayer, MCData);
   } else {
     bot.chat('all by myself');
+    moveToCoordinates(bot, SPAWN_POINT, MCData);
   }
 });
 
 
-function followPlayer (bot, player) {
-  const MCData = initMCData(bot.version);
+function followPlayer (bot, player, MCData) {
   const movements = new Movements(bot, MCData);
   const goal = new GoalFollow(player.entity, 1);
   // const goal = new GoalNear(SPAWN_POINT.x, SPAWN_POINT.y, SPAWN_POINT.z, 1);
@@ -48,6 +49,18 @@ function followPlayer (bot, player) {
 
   // bot stuffs
   bot.chat(`yooooooooooooooooooo ${player.username} !!!!!!`);
+  bot.pathfinder.setMovements(movements);
+  bot.pathfinder.setGoal(goal, true);
+}
+
+function moveToCoordinates (bot, coordinates, MCData) {
+  const movements = new Movements(bot, MCData);
+  const goal = new GoalNear(coordinates.x, coordinates.y, coordinates.z, 1);
+  
+  // movement stuffs
+  movements.canDig = false;
+
+  // bot stuffs
   bot.pathfinder.setMovements(movements);
   bot.pathfinder.setGoal(goal, true);
 }
